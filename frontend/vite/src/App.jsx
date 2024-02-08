f7183a4a9082f0dd0665779efdff3cb90623f00a
tulik <tolqinmirsaliyev@gmail.com>
2024-02-08 10:23:08 +0500

    ketti

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Routes , Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div><h1>salomlar o'zgardi</h1></div>
+
   )
 }
 
