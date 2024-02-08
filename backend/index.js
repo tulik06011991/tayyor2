67d3af2a2949856e89e66bbbc2669e96045bb263
tulik06011991 <149315647+tulik06011991@users.noreply.github.com>
2024-02-08 11:12:08 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() =>console.log(`connected  to  oo database`))
+.then(() =>console.log(`connected  to database`))
 .catch((error) =>console.log(error))
 
 PORT = process.env.PORT
