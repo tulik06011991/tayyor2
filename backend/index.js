5cb5b3b05aeb644ded98c001c1fcc000317f6974
tulik06011991 <149315647+tulik06011991@users.noreply.github.com>
2024-02-08 10:36:04 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 
 PORT = process.env.PORT
