248883805ed969aa8d817f857704b0bba587e730
tulik <tolqinmirsaliyev@gmail.com>
2024-02-08 11:49:41 +0500

    hh

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -7,25 +7,24 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand" to="/">Menu</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Features</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Pricing</Link>
+          <Link className="nav-link active" aria-current="page" to="/main">Main</Link>
         </li>
        
       </ul>
     </div>
   </div>
+  <span>
+          <Link  to="/">Features</Link>
+    
+          <Link  style={{margin: '10px 20px'}} to="/">Pricing</Link>
+  </span>
 </nav>
 
     </div>
