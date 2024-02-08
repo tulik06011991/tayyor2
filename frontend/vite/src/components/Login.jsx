3a08a679e6db3c78506ecbbc5a006b3bbba9c49b
tulik <tolqinmirsaliyev@gmail.com>
2024-02-08 12:46:39 +0500

    ketti

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -7,14 +7,10 @@ const Login = () => {
         <div className="row">
           <div className="col-sm-10 col-md-6 col-lg-4 mx-auto">
             <form>
+              
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"/>
+                <label for="exampleInputPassword1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputPassword1"/>
               </div>
               <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+    <div className="container">
+        <div className="row">
+          <div className="col-sm-10 col-md-6 col-lg-4 mx-auto">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Username</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1"/>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1"/>
+              </div>
+              
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
