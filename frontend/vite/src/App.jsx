a98158210f97b1fb04a5cf0501c697c7df3405c8
tulik <tolqinmirsaliyev@gmail.com>
2024-02-08 11:41:28 +0500

    ketti

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -4,11 +4,23 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 import './App.css'
 import {Routes , Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Register from './components/Register'
+import Logout from './components/Logout'
+import Main from './components/Main'
+import Menu from './components/Menu'
 
 const App = () => {
   return (
    <>
    <Navbar/>
+   <Routes>
+    <Route path='/login' element = {<Login/>}/>
+    <Route path='/register' element = {<Register/>}/>
+    <Route path='/logout' element = {<Logout/>}/>
+    <Route path='/main' element = {<Main/>}/>
+    <Route path='/' element = {<Menu/>}/>
+   </Routes>
    </>
 
   )
