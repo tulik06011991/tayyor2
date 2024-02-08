60ab44d71b7611e7267e08b507ce78a797307523
tulik <tolqinmirsaliyev@gmail.com>
2024-02-08 10:29:18 +0500

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+PORT = process.env.PORT
+
+
+
+app.listen(PORT, console.log(`server running on port ${PORT}`))
