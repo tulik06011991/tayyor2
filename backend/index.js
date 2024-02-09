75fe9dd0e1510d4361db5fbff26f6da0e9591ff2
tulik <tolqinmirsaliyev@gmail.com>
2024-02-09 09:03:10 +0500

    kettttiiii

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,23 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+const login = require("./Routes/Login")
+const register = require('./Routes/register')
+
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use("/auth", login())
+app.use("/auth", register())
+
+
+
+
+
+
+
 
 
 mongoose.connect(process.env.MONGO_URL)
@@ -11,9 +27,7 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((error) =>console.log(error))
 
 PORT = process.env.PORT
-app.get('/' , (req, res) =>{
-    res.send(` ishladi   urra`)
-})
+
 
 
 app.listen(PORT, console.log(`server running on port ${PORT}`))
