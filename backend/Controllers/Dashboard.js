1722d9ac62a0fc6c905c9b8f738a503a13bbe7ca
tulik <tolqinmirsaliyev@gmail.com>
2024-02-09 08:59:35 +0500

    ketttiiii

---
+++ backend/Controllers/Dashboard.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+
+const dashboard = (req, res) =>{
+    res.status(200).json(token)
+}
+
+module.exports = dashboard
