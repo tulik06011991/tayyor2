30475b4db2a29215b45e9b6896acc4370398d05b
tulik <tolqinmirsaliyev@gmail.com>
2024-02-09 09:48:35 +0500

    c xzcxzc

--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -18,17 +18,17 @@ const Register = () => {
           <div className="col-sm-10 col-md-6 col-lg-4 mt-5 mx-auto">
             <form  onSubmit={handleSubmit}>
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
+                <label htmlFor="exampleInputEmail1" class="form-label">Username</label>
                 <input type="text" class="form-control" onChange={((e) =>setUsername(e.target.value))} id="exampleInputEmail1" aria-describedby="emailHelp"/>
                   <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Email</label>
+                <label htmlFor="exampleInputPassword1" class="form-label">Email</label>
                 <input type="email" class="form-control"   onChange={((e) =>setEmail(e.target.value))}  id="exampleInputPassword1"/>
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control"  onChange={((e) =>setPassword(e.target.value))} id="exampleInputPassword1"/>
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
