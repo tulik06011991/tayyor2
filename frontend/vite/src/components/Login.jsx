8e62bf8083548d25ac5f5895c8d8d55abfa8d0c2
tulik <tolqinmirsaliyev@gmail.com>
2024-02-09 09:39:05 +0500

    ketti

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+
+  
   return (
     <>
       <div className="container">
--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <>
     <div className="container">
@@ -15,10 +18,12 @@ const Register = () => {
               <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1"/>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1"/>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
               </div>
               
               <button type="submit" class="btn btn-primary">Submit</button>
