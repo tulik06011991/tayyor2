bb5e08a939fdfc84b1139b11b7b39d7875ff2772
tulik <tolqinmirsaliyev@gmail.com>
2024-02-10 18:49:06 +0300

    ok1234

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -19,7 +19,9 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/register">Register</Link>
             </li>
-          
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
           </ul>
         </div>
       </div>
