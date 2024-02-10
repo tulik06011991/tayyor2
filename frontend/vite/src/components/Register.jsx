97c96dd6ac386a532ec0fa9e7e69681cc2c39f77
tulik <tolqinmirsaliyev@gmail.com>
2024-02-10 07:45:17 +0300

    ggggggg

--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -1,51 +1,61 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
+
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault()
     try {
-      const response  = axios.post(`http://localhost:5000/auth/register`,{
+      const response  = await axios.post(`http://localhost:5000/auth/register`,{
         username,
         email,
         password
       })
+      if(response.data.status === 200){
+        toast.success(`muvaffaqiyatli kirdingiz`)
+      }
       
       console.log(response)
       
     } catch (error) {
-      
+      if(error.response.status === 400){
+        toast.error("bu foydalanuvchi avval ro'yxatdan o'tgan")
+      }
+      else if(error.response.status === 401){
+        toast.error('siz hamma bo"sh joylarni to"ldirmadingiz')
+      }
     }
   }
 
-  
   return (
     <>
     <div className="container">
         <div className="row">
           <div className="col-sm-10 col-md-6 col-lg-4 mt-5 mx-auto">
-            <form  onSubmit={handleSubmit}>
-              <div class="mb-3">
-                <label htmlFor="exampleInputEmail1" class="form-label">Username</label>
-                <input type="text" class="form-control" onChange={((e) =>setUsername(e.target.value))} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputUsername" className="form-label">Username</label>
+                <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} id="exampleInputUsername" aria-describedby="emailHelp"/>
+                <div className="form-text">We'll never share your email with anyone else.</div>
               </div>
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Email</label>
-                <input type="email" class="form-control"   onChange={((e) =>setEmail(e.target.value))}  id="exampleInputPassword1"/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} id="exampleInputEmail1"/>
+                <div className="form-text">We'll never share your email with anyone else.</div>
               </div>
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control"  onChange={((e) =>setPassword(e.target.value))} id="exampleInputPassword1"/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} id="exampleInputPassword1"/>
+                <div className="form-text">We'll never share your email with anyone else.</div>
               </div>
               
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
 
           </div>
