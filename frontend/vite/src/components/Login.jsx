e15281c4673260778355a1b82be371876e429c13
tulik <tolqinmirsaliyev@gmail.com>
2024-02-10 18:01:54 +0300

    ok3

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -1,47 +1,49 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
-  const navigate = useNavigate()
-  const [email, setEmail] = useState('')
-  const [password , setPassword] = useState('')
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleLogin = async (e) =>{
-    e.preventDefault()
-    await axios.post(`http://localhost:5000/auth/login`, {email, password})
-    .then((response) =>{
-      navigate('/main')
-    })
-    .catch((error) =>console.log(error))
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:5000/auth/login`, { email, password });
+      navigate('/main');
+    } catch (error) {
+      if (error.response.status === 400) {
+        toast.error(`Login yoki parol xato`);
+      } else if (error.response.status === 401) {
+        toast.error(`hamm abo'sh joylarni to'ldiring`);
+      }
+    }
+  };
 
-  
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-sm-10 col-md-6 col-lg-4 mt-5 mx-auto">
             <form onSubmit={handleLogin}>
-              
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Email</label>
-                <input type="email"  onChange={(e) =>setEmail(e.target.value)} class="form-control" id="exampleInputPassword1"/>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="email" />
               </div>
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" onChange={(e) =>setPassword(e.target.value)} class="form-control" id="exampleInputPassword1"/>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="password" />
               </div>
-              
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-
           </div>
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
