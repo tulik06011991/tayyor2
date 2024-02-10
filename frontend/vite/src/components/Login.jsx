38078f829218ec691c6f20719b6851339ce0ad1d
tulik <tolqinmirsaliyev@gmail.com>
2024-02-10 18:47:26 +0300

    ok12

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -13,8 +13,11 @@ const Login = () => {
     e.preventDefault();
     try {
       await axios.post(`http://localhost:5000/auth/login`, { email, password });
-      toast.success(`saytga ulandingiz`)
-      navigate('/main');
+      if (response.data.status === 200) {
+        toast.success(`saytga ulandingiz`)
+        navigate('/main');
+        
+      }
     } catch (error) {
       if (error.response && error.response.status === 400) {
         toast.error(`Login yoki parol xato`);
