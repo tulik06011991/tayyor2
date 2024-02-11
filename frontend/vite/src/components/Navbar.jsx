b8377404989d28c24350eebf3ab1ccf090598bcd
tulik <tolqinmirsaliyev@gmail.com>
2024-02-11 21:39:54 +0300

    fhhghfgh

--- frontend/vite/src/components/Navbar.jsx
+++ frontend/vite/src/components/Navbar.jsx
@@ -7,37 +7,34 @@ const Navbar = () => {
   
   return (
     <div>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <Link className="navbar-brand" to="/">Menu</Link>
-        </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-          <ul className="navbar-nav  collapse navbar-collapse justify-content-between">
-            {user ? (
-              <li className="nav-item">
-                <button>
-                <Link className="nav-link" to="/logout">Logout</Link>
-
-                Logout</button>
-              </li>
-            ) : (
-              <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Menu</Link>
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul className="navbar-nav">
+              {user ? (
                 <li className="nav-item">
-                  <Link className="nav-link" to="/register">Register</Link>
+                  <button className="nav-link" onClick={() => { /* Logout funksiyasini chaqiring */ }}>Logout</button>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">Login</Link>
-                </li>
-              </>
-            )}
-          </ul>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/register">Register</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 }
