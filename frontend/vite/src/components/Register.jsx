7fd1a9b72899e6bfddb871cd6a9385995892cc7b
tulik <tolqinmirsaliyev@gmail.com>
2024-02-12 10:57:57 +0500

    dsadsa

--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -6,10 +6,10 @@ const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const response  = axios.post(`http://localhost:5000/auth/register`,{
+      const response = await axios.post(`http://localhost:5000/auth/register`, {
         username,
         email,
         password
@@ -18,35 +18,34 @@ const Register = () => {
       console.log(response)
       
     } catch (error) {
-    console.log(error)
+      console.log(error)
+    }
   }
 
-  
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="row">
           <div className="col-sm-10 col-md-6 col-lg-4 mt-5 mx-auto">
-            <form  onSubmit={handleSubmit}>
-              <div class="mb-3">
-                <label htmlFor="exampleInputEmail1" class="form-label">Username</label>
-                <input type="text" class="form-control" onChange={((e) =>setUsername(e.target.value))} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+                <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Email</label>
-                <input type="email" class="form-control"   onChange={((e) =>setEmail(e.target.value))}  id="exampleInputPassword1"/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Email</label>
+                <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} id="exampleInputPassword1"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
-              <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control"  onChange={((e) =>setPassword(e.target.value))} id="exampleInputPassword1"/>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} id="exampleInputPassword1"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
               
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-
           </div>
         </div>
       </div>
