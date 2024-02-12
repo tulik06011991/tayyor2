cbee6bb0b0a5b2c88b344a1139527f0751e77d1c
tulik <tolqinmirsaliyev@gmail.com>
2024-02-12 10:11:32 +0500

    dsdsad

--- frontend/vite/src/components/Login.jsx
+++ frontend/vite/src/components/Login.jsx
@@ -12,11 +12,11 @@ const Login = () => {
             <form>
               
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Email</label>
+                <label htmlFor="exampleInputPassword1" class="form-label">Email</label>
                 <input type="email" class="form-control" id="exampleInputPassword1"/>
               </div>
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1"/>
               </div>
               
