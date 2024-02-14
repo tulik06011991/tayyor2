33bf8c0627c1962f9fa824da373a2540dfb4d1a4
tulik <tolqinmirsaliyev@gmail.com>
2024-02-14 09:59:47 +0500

    iiiishladi

--- frontend/vite/src/components/Register.jsx
+++ frontend/vite/src/components/Register.jsx
@@ -1,56 +1,58 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const Register = () => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     try {
+      // Serverga so'rovni yuborish
       const response = await axios.post(`http://localhost:5000/auth/register`, {
         username,
         email,
         password
-      })
+      });
       
-      console.log(response)
-      
+      console.log(response);
+      // Ma'lumotlar muvaffaqiyatli yuborildi
+      toast.success('Ro\'yxatga olingan!');
     } catch (error) {
-      if (error.response && error.response.status === 400) {
-        toast.error(`Login yoki parol xato`);
-      } else if ( error.response.status === 401) {
-        toast.error(`hamma bo'sh joylarni to'ldiring`);
+      // Serverdan kelgan xato
+      if (error.response.status === 400) {
+        toast.error('Bu foydalanuvchi ro\'yxatdan o\'tgan');
+      } else if (error.response.status === 401) {
+        toast.error('Hamma maydonlarni to\'ldiring');
+      } else {
+        toast.error('Xatolik yuz berdi');
       }
     }
   }
 
   return (
     <>
+      <ToastContainer />
       <div className="container">
         <div className="row">
           <div className="col-sm-10 col-md-6 col-lg-4 mt-5 mx-auto">
             <form onSubmit={handleSubmit}>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
-                <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <label htmlFor="usernameInput" className="form-label">Username</label>
+                <input type="text" className="form-control" value={username} onChange={(e) => setUsername(e.target.value)} id="usernameInput" aria-describedby="usernameHelp"/>
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Email</label>
-                <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} id="exampleInputPassword1"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <label htmlFor="emailInput" className="form-label">Email</label>
+                <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} id="emailInput"/>
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} id="exampleInputPassword1"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <label htmlFor="passwordInput" className="form-label">Password</label>
+                <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} id="passwordInput"/>
               </div>
-              
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           </div>
@@ -60,4 +62,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default Register;
