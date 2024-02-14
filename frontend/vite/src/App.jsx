1c367b0d353ae1d039a16fa1bd9e2443d0034153
tulik06011991 <149315647+tulik06011991@users.noreply.github.com>
2024-02-14 06:08:36 +0300

    Update App.jsx

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,52 +1,55 @@
-// App.js
-import React, { useEffect, useState, createContext } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/Dashboard';
-import Menu from './components/Menu';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import './App.css'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Register from './components/Register'
+import Dashboard from './components/Dashboard'
+import Menu from './components/Menu'
+import axios from 'axios'
+import { createContext } from 'react'
 
-export const userContext = createContext();
+export const userContext = createContext()
 
 const App = () => {
-  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null) // user'ning qiymati null bo'lishi
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/dashboard',
-        {
-           headers: {
-             Authorization: `Bearer ${localStorage.getItem('token')}`,
-           },
-        });
-        setUserData(response.data);
-      } catch (error) {
-        console.error('Foydalanuvchi ma\'lumotlarini olishda xato:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+    const token = localStorage.getItem('token') // tokenni olish
+    if (token) {
+      axios.get('http://localhost:5000/auth/dashboard', {
+        headers: {
+          Authorization: `Bearer ${token}` // tokenni request headeriga qo'shish
+        }
+      })
+        .then((response) => {
+          setUser(response.data)
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error.message)
+        })
+    } else {
+      console.log(`loginni kiriting`)// agar token mavjud emas bo'lsa, login sahifasiga navigatsiya qilish
+    }
+  }, [navigate]) // navigate o'zgarishlarini eslatish
 
   return (
-    <userContext.Provider value={userData}>
-      <>
+    <>
+      <userContext.Provider value={user}>
         <Navbar />
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Menu />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/' element={<Menu />} />
         </Routes>
-      </>
-    </userContext.Provider>
-  );
-};
+      </userContext.Provider>
+    </>
+  )
+}
 
-export default App;
+export default App
